1b2e70a36a0a838bfe8acd09f60d726c3282c574
Elrendar <guuto9@gmail.com>
2022-07-18 12:42:42 +0900

    Sprin 과제

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var bus1 = new Bus();
+        var bus2 = new Bus();
+
+        bus1.AddPassengers(2);
+        bus1.ConsumeGas(50);
+        bus1.ChangeState(Transport.State.garage);
+        bus1.RefuelGas(10);
+        bus1.ChangeState(Transport.State.running);
+        bus1.AddPassengers(45);
+        bus1.AddPassengers(5);
+        bus1.ConsumeGas(55);
+
+        System.out.println("----------------------------------");
+
+        var taxi = new Taxi();
+        var taxi2 = new Taxi();
+
+        taxi.AddPassengers(2, "서울역", 2);
+        taxi.ConsumeGas(80);
+        taxi.AddPassengers(5);
+        taxi.AddPassengers(3, "구로디지털단지역", 12);
+        taxi.ConsumeGas(20);
     }
 }
